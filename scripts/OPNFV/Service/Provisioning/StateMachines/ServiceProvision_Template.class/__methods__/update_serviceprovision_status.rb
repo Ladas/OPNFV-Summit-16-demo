990cd24a8c2d5a6dcc29bb1c192600c3cb7ce3a4
Ladislav Smola <lsmola@redhat.com>
2016-06-16 14:29:10 +0200

    Show current status in the request list
    We need to modify update_serviceprovision_status, to show
    status of order_network_service state machine.

---
+++ scripts/OPNFV/Service/Provisioning/StateMachines/ServiceProvision_Template.class/__methods__/update_serviceprovision_status.rb
@@ -0,0 +1,21 @@
+#
+# Description: This method updates the service provisioning status
+# Required inputs: status
+#
+
+prov = $evm.root['service_template_provision_task']
+
+unless prov
+  $evm.log(:error, "Service Template Provision Task not provided")
+  exit(MIQ_STOP)
+end
+
+# Get status from input field status
+status = $evm.inputs['status']
+
+# Update Status for on_entry,on_exit
+if $evm.root['ae_result'] != 'retry'
+  prov.message = status
+  prov.miq_request.user_message = status
+end
+
