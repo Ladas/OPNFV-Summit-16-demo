85b4ee7aeb69b117548e06bd1d191d93cf8d79f1
Ladislav Smola <lsmola@redhat.com>
2016-07-08 18:40:54 +0200

    Pass cluster info to ansible
    Cluster info has IPs of all CPs for each vm and CIDRs for
    each CP for vim_id

--- scripts/OPNFV/NetworkServices/Methods.class/__methods__/run_ansible_jobs.rb
+++ scripts/OPNFV/NetworkServices/Methods.class/__methods__/run_ansible_jobs.rb
@@ -12,7 +12,6 @@ def launch_ansible_job(configuration_manager, network_service, parent_service, t
   end
   $evm.log(:info, "Running Ansible Tower template: #{template.name} on VMs: #{vm_names} with properties: #{properties}")
   
-  
   orchestration_service.job_template          = template
   orchestration_service.configuration_manager = configuration_manager
   orchestration_service.job_options           = {:limit => vm_names, :extra_vars => properties}
@@ -28,6 +27,17 @@ def get_vpn_server_ip(parent_service)
   vm.floating_ips.detect { |x| x.network_port.cloud_subnets.detect { |subnet| subnet.name == 'CloudExternal' }}.try(:address)
 end  
 
+def cps_for_id(network_service, id)
+  # Returns list of capabilities for each VNF, e.g.: ["CloudExternal", "VL1", "VL2", "net_mgmt"]
+  cps = []
+  network_service.direct_service_children.detect { |x| x.name == 'VNFs' }.direct_service_children.each do |vnf_service| 
+    if JSON.parse(vnf_service.custom_get('properties'))['id'].to_s == id.to_s
+      cps = JSON.parse(vnf_service.custom_get('requirements')).map { |x| x.values.first }.compact
+    end
+  end
+  cps
+end  
+
 begin
   nsd = $evm.get_state_var(:nsd)
   $evm.log("info", "Listing nsd #{nsd}")
@@ -38,7 +48,30 @@ begin
   parent_service = $evm.root['service_template_provision_task'].destination
   parent_service.name = $evm.root.attributes['dialog_service_name']
   
+  network_service = $evm.vmdb('service', $evm.root.attributes['dialog_network_service'])
+  
   vpn_server_ip = get_vpn_server_ip(parent_service)
+  
+  cluster = {}
+  parent_service.direct_service_children.each do |vnf_service|
+    json_properties = vnf_service.custom_get('properties') || '{}'
+    properties = JSON.parse(json_properties)
+    id = properties['id']
+    vim_id = properties['vim_id']
+    next unless id
+    
+    cluster[id] = {}
+    cluster[vim_id] ||= {}
+    # TODO handle more vms per VNF
+    vm = vnf_service.vms.first
+    cps_for_id(network_service, id).each do |connection_point|
+      network_port = vm.network_ports.detect { |x| x.cloud_subnets.detect { |subnet| subnet.name.include?(connection_point) }}
+      cluster[id][connection_point] = {
+        :fixed_ips    => network_port.try(:fixed_ip_addresses), 
+        :floating_ips => network_port.try(:floating_ip_addresses)}
+      cluster[vim_id][connection_point] ||= {:cidrs => network_port.cloud_subnets.collect(&:cidr)} if network_port
+    end
+  end
   
   parent_service.direct_service_children.each do |vnf_service|
     # There can be more types of services, we are interested in services with ansible job name defined
@@ -46,6 +79,7 @@ begin
     json_properties = vnf_service.custom_get('properties') || '{}'
     properties = JSON.parse(json_properties) 
     properties['vpn_server_ip'] = vpn_server_ip
+    properties['cluster']       = cluster
     
     ansible_manager_name = properties['ansible_vim_id']
     template_name        = properties['ansible_template_name']
