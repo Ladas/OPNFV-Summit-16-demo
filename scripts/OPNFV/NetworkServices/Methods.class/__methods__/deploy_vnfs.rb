f2f53090127fbf57b85bcbede68c67c32a7e4a98
Ladislav Smola <lsmola@redhat.com>
2016-09-12 10:02:08 +0200

    Use miq_server IP address for API requests
    Use miq_server IP address for API requests

--- scripts/OPNFV/NetworkServices/Methods.class/__methods__/deploy_vnfs.rb
+++ scripts/OPNFV/NetworkServices/Methods.class/__methods__/deploy_vnfs.rb
@@ -116,7 +116,7 @@ def get_template(orchestration_manager, network_service, parent_service, vnf_ser
               :ems_id       => orchestration_manager.id,
               :content      => YAML.dump(template_content)}
 
-  url     = 'http://localhost:3000/api/orchestration_templates'
+  url     = "#{$evm.root['miq_server'].ipaddress}/api/orchestration_templates"
   options = {:method     => :post,
              :url        => url,
              :verify_ssl => false,
@@ -124,10 +124,9 @@ def get_template(orchestration_manager, network_service, parent_service, vnf_ser
                              "resource" => resource}.to_json,
              :headers    => {"X-Auth-Token" => MIQ_API_TOKEN,
                              :accept        => :json}}
-  $evm.log("info", "LADASOR opts #{options}")
+  $evm.log("info", "Creating VNFd template #{options}")
 
   body = JSON.parse(RestClient::Request.execute(options))
-  $evm.log("info", "LADASOR resp #{body}")
 
   $evm.vmdb('orchestration_template_vnfd', body["results"].first["id"])
 end
