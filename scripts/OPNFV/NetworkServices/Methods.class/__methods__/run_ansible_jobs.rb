27afeb7cfc444b6375bb56ee911c01089db04c62
Ladislav Smola <lsmola@redhat.com>
2016-09-13 18:27:02 +0200

    Seems like Rails Rack has issues parsing complex json
    Seems like Rails Rack has issues parsing complex json, lets
    dump the body to json, so rack doesn't touch it

--- scripts/OPNFV/NetworkServices/Methods.class/__methods__/run_ansible_jobs.rb
+++ scripts/OPNFV/NetworkServices/Methods.class/__methods__/run_ansible_jobs.rb
@@ -20,7 +20,7 @@ def launch_ansible_job(configuration_manager, network_service, parent_service, t
               :job_template          => {:id => template.id},
               :configuration_manager => {:id => configuration_manager.id},
               :parent_service        => {:id => parent_service.id},
-              :job_options           => {:limit => vm_names, :extra_vars => properties},
+              :job_options           => {:limit => vm_names, :extra_vars => properties}.to_json,
               :display               => true}
 
   url     = "http://localhost:3000/api/services"
